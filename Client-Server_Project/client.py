30bcad9919480797951d6af472dba818bee70784
Mohamad <mohamadbader20@yahoo.com>
2020-10-19 04:24:34 +0100

    Update files of client and server

--- Client-Server_Project/client.py
+++ Client-Server_Project/client.py
@@ -1,13 +1,41 @@
 import boto3
+import time
+
 
 
 AWS_SQS_QUEUE_NAME = 'Mohamadsqs'
+QueuResultName= "ServerQueue"
+queueurlResultUrl= 'https://sqs.us-east-1.amazonaws.com/804385845436/ServerQueue'
+
 clientSQS = boto3.resource('sqs')
 queu = clientSQS.get_queue_by_name(QueueName=AWS_SQS_QUEUE_NAME)
 
 msg = input("inter your 20 numbers ")
+authorA=input("enter any personal ID ")
 
-response = queu.send_message(MessageBody = msg)
-print("your message id is "+ response['MessageId'])
+response = queu.send_message(MessageBody = msg,MessageAttributes={
+ 'Author': {
+    'StringValue': authorA,
+    'DataType': 'String' }
+
+})
+
+
+#### can canseble
+queueResult = clientSQS.get_queue_by_name(QueueName=QueuResultName)
+
+reciveResult = queueResult.receive_messages()
+print('you will get the response soon.... ')
+time.sleep(30)
+
+for message in queueResult.receive_messages():
+ print(message.body)
+    
+ if message.message_attributes is not None:
+            author_name = message.message_attributes.get('Author').get('StringValue')
+            if author_name == authorA:
+
+                    print(message.body)
+        ##
 
 
--- Client-Server_Project/server.py
+++ Client-Server_Project/server.py
@@ -17,20 +17,34 @@ queue = sqs.get_queue_by_name(QueueName=AWS_SQS_QUEUE_NAME)
 queueResult = sqs.get_queue_by_name(QueueName=QueuResultName)
 
 numberslist         = []
-for message in queue.receive_messages():
-    listnum = message.body
+x=0
+while x<=20:
+ for message in queue.receive_messages(MessageAttributeNames=['Author']):
+        
+            author_name1 = message.message_attributes.get('Author').get('StringValue') 
+    
+            listnum = message.body
+            
 
-    # Processing the message from the client
-    for nb in listnum.split():
-        numberslist.append(float(nb))
+            # Processing the message from the client
+            for nb in listnum.split():
+                numberslist.append(float(nb))
 
-    print('Sets of numbers received, {}!'.format(numberslist))
+            print('Sets of numbers received, {}!'.format(numberslist))
 
-    # Clacutation phase
-    min     = min(numberslist)
-    max     = max(numberslist)
-    mean    = statistics.mean(numberslist)
-    median  = statistics.median(numberslist)
-    print('the max number is: ',max,'the min number is: ',min,'the mean number is: ',mean,'the median number is: ',median ) 
-    servermessage= "'the max number is: ',max,'the min number is: ',min,'the mean number is: ',mean,'the median number is: ',median"
-    responseResult = queueResult.send_message(MessageBody= servermessage)
+            # Clacutation phase
+            min     = min(numberslist)
+            max     = max(numberslist)
+            mean    = statistics.mean(numberslist)
+            median  = statistics.median(numberslist)
+            
+            print('the max number is: ',max,'the min number is: ',min,'the mean number is: ',mean,'the median number is: ',median ) 
+            servermessage=  'the max number is: '+str(max)+'the min number is: '+str(min)+'the mean number is: '+str(mean)+'the median number is: '+str(median)
+            responseResult = queueResult.send_message(MessageBody= servermessage,MessageAttributes={
+            'Author': {
+                'StringValue': author_name1,
+                'DataType': 'String'
+            }})
+ x+=1   
+
+
