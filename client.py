34b59fa2dd5cb8ceec25b7eca49cc6109865d27d
jehadmelad <jd.melad@gmail.com>
2020-10-26 01:24:24 -0400

    version 2

--- client.py
+++ client.py
@@ -1,36 +1,106 @@
 import boto3
+import botocore.exceptions as bx
 from sys import argv
+import publicip
+import logging
+import time
+import random
 
+logFormat = "%(levelname)s   %(asctime)-15s %(filename)s[%(lineno)d] : %(message)s"
+logging.basicConfig(level=logging.INFO,format=logFormat)
+
+logger = logging.getLogger()
+
+
+
+
+author  = ''
 
 class main():
+    def __init__(self):
+        self.sqs = boto3.resource('sqs')
+        logger.info("Starting AWS service SQS....")
+        try:
+            if not self.sqs.get_queue_by_name(QueueName='requestQueue'):
+                self.request_Queue = self.sqs.create_queue(QueueName='requestQueue')
+            else:
+                print("The request queue is already exist!")
+        except bx.ClientError as e:
+            logger.debug(e)
+            
+    
+    
+    def send_msg(self, msg):
+        global author
+        author  = str(publicip.get())+str(random.randint(1, 1000))
+        request_Queue = self.sqs.get_queue_by_name(QueueName='requestQueue')
+        try:
+            self.req_smg = request_Queue.send_message(MessageBody=msg, MessageAttributes={'Author': {'StringValue': author,'DataType': 'String' }})
+            
+            print("Your request has been sent.\n")
 
-    sqs = boto3.resource('sqs')
-
-    req_Queue = sqs.create_queue(QueueName='requestQueue', Attributes={'DelaySeconds': '2'})
-    res_Queue = sqs.create_queue(QueueName='responseQueue', Attributes={'DelaySeconds': '2'})
-
-    # Get the queue
-    requestQueue = sqs.get_queue_by_name(QueueName='requestQueue')
-    responseQueue = sqs.get_queue_by_name(QueueName='responseQueue')
-
-    list_of_nb = str(input("""please enter a set of numbers, 'seperated by space! : """))
-    # TODO in case of entring string 
-    num = list_of_nb.split()
-    if len(num) <= 1:
-        print('Please, try to insert two number at least!')
-    else:
+        except bx.ClientError:
+            print(" here ")
+                    
+    def receive_msg(self):
+        global author
+        self.response_Queue = self.sqs.get_queue_by_name(QueueName='responseQueue')
+       
+        for answer in self.response_Queue.receive_messages(MessageAttributeNames=['Author']):
+           
+            if answer.message_attributes.get('Author').get('StringValue') == author:
+                print("this is      ",author)
+                answer_msg = answer.body
+                print(answer_msg)
+                answer.delete()
+                return 1
+            else:
         
-        # Create a new message to be sent 
-        req_smg = requestQueue.send_message(MessageBody=list_of_nb)
-        while responseQueue.receive_messages():
-            print("Wait for response . . . .")
-            for new in responseQueue.receive_messages():
-                answer_msg = new.body
-                new.delete()
-            print(answer_msg)
-
+                print("unable to find the author value....")
 
 
 
 if __name__ == "__main__":
-    main = main()
+    client = main()
+    msg = None
+    new_msg = ['']
+    while msg != "exit":
+        msg = input("""
+######################################################################\n
+Note: sepatated by space for example: 1 5 2.3 9 7.3 \n\
+---------------------------------------------------------------\n\
+To exist : type <exit>. \n\n
+To continue : please Enter your values to be calculated, Note: sepatated by space ? """)
+        try:
+            if msg != '':
+                new_msg = msg.split()
+                list(map(float, new_msg))
+                if len(new_msg) <= 3:
+                    msg = input("\n\
+-----------------------------------------------------------------------\n\
+You need to enter at least 10 numbers! Press ENTER to try again...!!! ")    
+                else:
+                    client.send_msg(msg)
+                    print("Wait for processing ...")
+                    while  client.receive_msg() != 1 :
+                        print("waiting a response......")
+            else:
+                msg = input("\n\
+-----------------------------------------------------------------------\n\
+Please set your values, example (1 2 3.3 8.2), Press ENTER to try again...!!! ")
+        except ValueError:
+            if msg != 'exit':
+                msg = input("\n\
+-----------------------------------------------------------------------\n\
+It seems you entred a sring value, Press ENTER to try again...!!!  ")
+            else:
+                print("\nYou are exiting the application.... ")
+                time.sleep(2)
+                break
+
+   
+    
+        
+
+
+
